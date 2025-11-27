670df0e008adbbbc60b29f35d35a10aa27e93679
moxar-tech <kyokuhairemark.com>
2025-11-27 07:53:53 +0300

    added imported file extension = ts in schema.prisma

--- app/generated/prisma/models.ts
+++ app/generated/prisma/models.ts
@@ -8,7 +8,7 @@
  *
  * 🟢 You can import this file directly.
  */
-export type * from './models/Account.js'
-export type * from './models/Session.js'
-export type * from './models/User.js'
-export type * from './commonInputTypes.js'
+export type * from './models/Account.ts'
+export type * from './models/Session.ts'
+export type * from './models/User.ts'
+export type * from './commonInputTypes.ts'
